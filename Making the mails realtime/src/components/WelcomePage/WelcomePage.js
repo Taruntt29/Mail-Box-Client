fd038875cbc39e2d1850cba0953c2d77eff32412
Taruntt29 <109497779+Taruntt29@users.noreply.github.com>
2022-09-04 17:03:53 +0530

    Add files via upload

---
+++ Making the mails realtime/src/components/WelcomePage/WelcomePage.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { authActions } from "../../context/authReducer";
+import CreateEmail from "./mailbox/CreateEmail";
+import Inbox from "./mailbox/Inbox";
+import Outbox from "./mailbox/Outbox";
+import classes from "./WelcomePage.module.css";
+const WelcomePage = () => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const userEmail = useSelector(state=>state.auth.email);
+  const [createMailOpen, setCreateMailOpen] = useState(false);
+  const [inboxOpen, setInboxOpen] = useState(true);
+  const [outboxOpen, setOutboxOpen] = useState(false);
+  const [unreadMessages, setUnreadMessages] = useState(0);
+
+  const createMailClickHandler = () => {
+    setInboxOpen(false);
+    setOutboxOpen(false);
+    setCreateMailOpen(true);
+  };
+  const inboxClickHandler = () => {
+    setOutboxOpen(false);
+    setCreateMailOpen(false);
+    setInboxOpen(true);
+  };
+  const outboxClickHandler = () => {
+    setCreateMailOpen(false);
+    setInboxOpen(false);
+    setOutboxOpen(true);
+  };
+  const logoutClickHandler = () => {
+    dispatch(authActions.logout());
+    history.replace('/login');
+  };
+
+  const setUnreadHandler = (data) => {
+    setUnreadMessages(data);  
+  }
+
+  return (
+    <div>
+      <div>
+        <h1>Welcome To your Mail Box</h1>
+        <span>{userEmail}</span>
+        <button onClick={logoutClickHandler}>Log Out</button>
+      </div>
+      <br/>
+      <div>
+        <div className={classes.sideNav}>
+          <button onClick={createMailClickHandler}>Create Email</button>
+          <br />
+          <button onClick={inboxClickHandler}>In Box</button>
+          <span>{unreadMessages}</span>
+          <br />
+          <button onClick={outboxClickHandler}>Out Box</button>
+        </div>
+        <div className={classes.mailBox}>
+          {createMailOpen && <CreateEmail />}
+          {inboxOpen && <Inbox />}
+          {inboxOpen && <Inbox setUnread={setUnreadHandler}/>}
+          {outboxOpen && <Outbox />}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default WelcomePage;
---
+++ Making the mails realtime/src/components/WelcomePage/mailbox/Inbox.js
@@ -0,0 +1,98 @@
+import React, { Fragment, useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import SingleMail from "./SingleMail";
+import { mailActions } from '../../../context/mailReducer';
+import { mailActions } from "../../../context/mailReducer";
+
+const Inbox = () => {
+const Inbox = (props) => {
+  const dispatch = useDispatch();
+  const [singleMail, setSingleMail] = useState("");
+  const emails =  useSelector(state=>state.mail.inbox);
+  const emails = useSelector((state) => state.mail.inbox);
+  const cleanUserEmail = useSelector((state) => state.auth.cleanEmail);
+
+  if (emails) {
+    props.setUnread(Object.keys(emails).reduce((p,key)=>{
+      if(emails[key].isRead) return p+1;
+      return p;
+    },0)-1)
+  }
+
+  // if(emails) props.setUnread()
+  useEffect(() => {
+    fetch(
+      `https://mail-box-client-3e006-default-rtdb.firebaseio.com/${cleanUserEmail}/inbox.json`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch(mailActions.setInbox(data));
+      });
+  }, [cleanUserEmail, dispatch]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetch(
+        `https://mail-box-client-3e006-default-rtdb.firebaseio.com/${cleanUserEmail}/inbox.json`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          dispatch(mailActions.setInbox(data));
+        });
+    }, 6000);
+    return () => clearInterval(interval);
+  }, [cleanUserEmail, dispatch]);
+
+  const openEmailClickHandler = (event) => {
+    setSingleMail({
+      email: emails[event.currentTarget.id],
+      ID: event.currentTarget.id,
+    });
+  };
+  const emailListJSX = emails ? (
+    <ul>
+      {Object.keys(emails).map((item) => (
+        <li
+          id={item}
+          onClick={openEmailClickHandler}
+          style={{
+            border: "2px solid black",
+            textAlign: "left",
+            listStyle: emails[item].isRead?'none':'',
+            listStyle: emails[item].isRead ? "none" : "",
+          }}
+          key={item}
+        >
+          {/* {<div style={{width: '10px', height:'10px', borderRadius: '50%', backgroundColor: 'red'}}/>} */}
+          <span style={{ paddingRight: "10px", textAlign: "left" }}>
+            From: {emails[item].from}
+          </span>
+          <span>Heading: {emails[item].heading}</span>
+        </li>
+      ))}
+    </ul>
+  ) : (
+    <p>No Emails Found</p>
+  );
+  const onSingleMailCloseHandler = () => {
+    setSingleMail("");
+  };
+  const onSingleMailDeleteHandler = (data) => {
+    dispatch(mailActions.setInbox(data));
+    setSingleMail("");
+  };
+  return (
+    <Fragment>
+      <h3>This is Inbox</h3>
+      {!singleMail && emailListJSX}
+      {singleMail && (
+        <SingleMail
+          onDelete={onSingleMailDeleteHandler}
+          onClose={onSingleMailCloseHandler}
+          data={singleMail}
+        />
+      )}
+    </Fragment>
+  );
+};
+export default Inbox;
