72c390dc0613fa26e42bc644f5b3094eb081ff29
Taruntt29 <109497779+Taruntt29@users.noreply.github.com>
2022-09-04 17:03:16 +0530

    Add files via upload

---
+++ login screen/src/App.js
@@ -0,0 +1,17 @@
+import { Route } from "react-router-dom";
+import AuthForm from "./components/Auth/AuthForm";
+import Welcome from "./components/Pages/Welcome";
+
+
+function App() {
+  return (
+    <>
+      <AuthForm></AuthForm>
+     <Route path='/' exact> <AuthForm></AuthForm></Route>
+      <Route path='/Welcome'>
+        <Welcome/>
+      </Route>
+    </>
+  );
+}
+export default App;
